766f9c1db9eca08a5406d5ab3dcc2d55700fa624
Mujo Ramic <32583467+MrNoOne007@users.noreply.github.com>
2018-05-18 18:14:25 +0200

    Update KorisnikDAOInterface.java

--- DAO/KorisnikDAOInterface.java
+++ DAO/KorisnikDAOInterface.java
@@ -1,12 +1,1 @@
-package DAO;
 
-import java.sql.SQLException;
-import DTO.Korisnici;
-
-public interface KorisnikDAOInterface {
-
-	public void addUser() throws SQLException;
-
-	public String getUser(String userName) throws SQLException;
-
-}
